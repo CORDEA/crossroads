843ef8feed9a5e0667f7e1283214efbda1ba8a45
Yoshihiro Tanaka <contact@cordea.jp>
2021-02-02 16:23:15 +0900

    provide model

--- lib/app.dart
+++ lib/app.dart
@@ -1,25 +1,30 @@
+import 'package:crossroads/models/stock.dart';
 import 'package:crossroads/ui/calculator.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: Colors.pink[300],
-        accentColor: Colors.amber[300],
-        textSelectionTheme: TextSelectionThemeData(
-          cursorColor: Colors.pink[300],
+    return ChangeNotifierProvider(
+      create: (context) => StockModel(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primaryColor: Colors.pink[300],
+          accentColor: Colors.amber[300],
+          textSelectionTheme: TextSelectionThemeData(
+            cursorColor: Colors.pink[300],
+          ),
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.pink[300],
+            textTheme: ButtonTextTheme.primary,
+            minWidth: 64,
+            height: 44,
+          ),
         ),
-        buttonTheme: ButtonThemeData(
-          buttonColor: Colors.pink[300],
-          textTheme: ButtonTextTheme.primary,
-          minWidth: 64,
-          height: 44,
-        ),
+        home: Calculator(title: 'Flutter Demo Home Page'),
       ),
-      home: Calculator(title: 'Flutter Demo Home Page'),
     );
   }
 }
