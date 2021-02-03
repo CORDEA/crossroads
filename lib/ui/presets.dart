90299df95ed15dd539f70276896a18589a230c82
Yoshihiro Tanaka <contact@cordea.jp>
2021-02-03 15:26:40 +0900

    define item

--- lib/ui/presets.dart
+++ lib/ui/presets.dart
@@ -7,11 +7,37 @@ class Presets extends StatelessWidget {
       appBar: AppBar(
         title: Text('Presets'),
       ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(16),
-          child: Center(),
-        ),
+      body: ListView(
+        children: [
+          _PresetsItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class _PresetsItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 16, right: 16),
+      constraints: BoxConstraints(
+        minHeight: 72,
+        minWidth: double.infinity,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '',
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+          Text(
+            '',
+            style: Theme.of(context).textTheme.caption,
+          ),
+        ],
       ),
     );
   }
