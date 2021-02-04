7538f701af01486b3cc028d4e050cab82bea66b1
Yoshihiro Tanaka <contact@cordea.jp>
2021-02-04 17:13:30 +0900

    display items

--- lib/ui/presets.dart
+++ lib/ui/presets.dart
@@ -1,24 +1,44 @@
+import 'package:crossroads/bloc/preset_bloc.dart';
+import 'package:crossroads/models/preset.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Presets extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    context.read<PresetBloc>().update();
     return Scaffold(
       appBar: AppBar(
         title: Text('Presets'),
       ),
-      body: ListView(
-        children: [
-          _PresetsItem(),
-        ],
-      ),
+      body: _PresetsList(),
+    );
+  }
+}
+
+class _PresetsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var length = context.select<PresetBloc, int>(
+      (value) => value.presets.length,
+    );
+    return ListView.builder(
+      itemBuilder: (context, index) => _PresetsItem(index: index),
+      itemCount: length,
     );
   }
 }
 
 class _PresetsItem extends StatelessWidget {
+  final int index;
+
+  _PresetsItem({Key key, @required this.index}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    var item = context.select<PresetBloc, Preset>(
+      (value) => value.presets[index],
+    );
     return Container(
       padding: EdgeInsets.only(left: 16, right: 16),
       constraints: BoxConstraints(
@@ -30,7 +50,7 @@ class _PresetsItem extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
-            '',
+            item.title,
             style: Theme.of(context).textTheme.subtitle1,
           ),
           Text(
