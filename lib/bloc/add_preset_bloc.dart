c70af350bce2374b6ca290b3338452106c87eccc
Yoshihiro Tanaka <contact@cordea.jp>
2021-02-04 21:07:43 +0900

    implement bloc

---
+++ lib/bloc/add_preset_bloc.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/foundation.dart';
+
+class AddPresetBloc extends ChangeNotifier {
+  String _title;
+  double _purchaseFeeRate;
+  bool _hasPurchaseFeeRateError;
+  double _saleFeeRate;
+  bool _hasSaleFeeRateError;
+
+  void onTitleChanged(String value) {
+    _title = value;
+  }
+
+  void onPurchaseFeeRateChanged(String value) {
+    var v = double.tryParse(value);
+    _hasPurchaseFeeRateError = v == null;
+    _purchaseFeeRate = v ?? 0;
+    notifyListeners();
+  }
+
+  void onSaleFeeRateChanged(String value) {
+    var v = double.tryParse(value);
+    _hasSaleFeeRateError = v == null;
+    _saleFeeRate = v ?? 0;
+    notifyListeners();
+  }
+
+  void submit() {
+    if (_hasPurchaseFeeRateError || _hasSaleFeeRateError) {
+      return;
+    }
+  }
+}
