7d1abd75c77fc9b343783e4dbb42983873f3ff4b
Yoshihiro Tanaka <contact@cordea.jp>
2021-02-04 21:01:21 +0900

    update layout

--- lib/ui/add_preset.dart
+++ lib/ui/add_preset.dart
@@ -1,3 +1,4 @@
+import 'package:crossroads/ui/outlined_text_field.dart';
 import 'package:flutter/material.dart';
 
 class AddPreset extends StatelessWidget {
@@ -7,7 +8,38 @@ class AddPreset extends StatelessWidget {
       appBar: AppBar(
         title: Text('Add preset'),
       ),
-      body: Center(),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            OutlinedTextField(
+              text: 'Title',
+              errorText: '',
+              onChanged: (text) {},
+            ),
+            const SizedBox(height: 16),
+            OutlinedTextField(
+              text: 'Purchase fee rate',
+              errorText: '',
+              onChanged: (text) {},
+            ),
+            const SizedBox(height: 16),
+            OutlinedTextField(
+              text: 'Sale fee rate',
+              errorText: '',
+              onChanged: (text) {},
+            ),
+            const SizedBox(height: 32),
+            Align(
+              alignment: Alignment.centerRight,
+              child: RaisedButton(
+                child: const Text('Add'),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
